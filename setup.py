ecf4f4aa37f34690eabd34c35aa5bb4107288b6e
Patrik Dufresne <patrik@ikus-soft.com>
2025-03-14 08:15:06 -0400

    Replace setup.cfg by pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (C) 2025 IKUS Software. All rights reserved.
-# IKUS Software inc. PROPRIETARY/CONFIDENTIAL.
-# Use is subject to license terms.
-
-import setuptools
-
-setuptools.setup(use_scm_version=True)
